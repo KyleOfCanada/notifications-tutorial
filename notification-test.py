61063b5002fcd215f09127a3d7497f437bd781c9
Kyle Roddick <kyle.roddick@gmail.com>
2021-07-01 22:50:28 -0300

    add notification test with IFTTT

---
+++ notification-test.py
@@ -0,0 +1,12 @@
+from secrets import web_hook
+import requests
+
+
+# imported web_hook is
+# def web_hook(event):
+#     url = f'https://maker.ifttt.com/trigger/{event}/with/key/{my_key}'
+#     return url
+
+
+ifttt_web_hook = web_hook('test_event')
+requests.post(ifttt_web_hook)
