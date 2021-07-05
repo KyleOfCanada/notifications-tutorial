3e6b516525f344964156afb4277f46a9a5828e96
Kyle Roddick <kyle.roddick@gmail.com>
2021-07-05 15:22:34 -0300

    test spaces in url

--- IFTTT notification.R
+++ IFTTT notification.R
@@ -4,8 +4,8 @@ library(httr)
 my_key <- Sys.getenv('IFTTT_KEY') # from a GitHub secret
 print(nchar(my_key))
 event <- 'R_post'
-value1 <- 'test'
-value2 <- 'GitHubAction'
+value1 <- 'space+test'
+value2 <- 'GitHub%20Action'
 
 webhookURL <- paste0('https://maker.ifttt.com/trigger/', event, '/with/key/', my_key, '?&value1=', value1, '&value2=', value2)
 
